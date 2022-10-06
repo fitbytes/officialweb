5e8b14743b115ada5bff8aa3f712f5496b5f3ef6
junaid bashir <89861553+Junaid-Bashir-Kernel@users.noreply.github.com>
2022-10-06 20:34:17 +0530

    start

---
+++ pages/privacy.js
@@ -0,0 +1,92 @@
+export default function Privacy(){
+
+return (
+ <div>  
+  <h1 style={{color:"tomato",textAlign:"center"}}>PRIVACY POLICY <i>@fitbytes</i></h1>
+<ul>
+<li>GENERAL</li>
+
+<li>a. This Website with the URL of https://fitbytes.github.io/ ("Website/Site") is operated by FitBytes ("We/Our/Us"). We are committed to protecting and respecting your privacy. We do collect your personal information and process your personal data in accordance with the IT Act, 2000 (21 of 2000) and other national and state laws which relate to the processing of personal data. Please read the following carefully to understand our views and practices regarding your personal data.</li>
+
+<li>b. We collect yo
+ur personal information in order to provide and continually improve our products and services.</li>
+
+<li>c. Our privacy policy is subject to change at any time without notice. To make sure you are aware of any changes, please review this policy periodically.</li>
+
+<li>d. All partner firms and any third-party working with or for Us, and who have access to personal information, will be expected to read and comply with this policy.</li>
+      
+  
+ 
+  <li>HOW WE COLLECT THE INFORMATION</li>
+
+<li>a. From you directly and through this Site: We may collect information through the Website when you visit. The data we collect depends on the context of your interactions with our Website.</li>
+
+<li> Information about your  via our service provider fitbit </li>
+
+     
+   <li> INFORMATION WE COLLECT</li>
+
+<li>a. We collect information primarily to provide services to all of our customers.</li>
+
+<li>b. We collect the following information from you when you use or signup on our Website:</li>
+
+<li>user metrics derived from our partner Fitbit as :</li>
+<li>Activity tracking(all activities supported by fitbit tracker)</li>
+<li>Sleep</li>
+<li>Calories</li>
+<li>Heart (bpm)</li>
+     
+ <li>c. We do collect the following sensitive personal information from you when you use or sign up on our Website:</li>
+
+<li>Fitness data</li>
+<li>Activity data</li>
+    
+  
+   <li>b. We use the information collected through this website as described in this policy and we may use your information to:</li>
+
+
+<li>1 . Understand and enhance your experience using our Site, products, and services</li>
+
+<li>2 . Personalize our products or services and make recommendations;</li>
+
+<li>3 . Provide and deliver products and services you request;</li>
+ <li>4 . Create and manage the online accounts you manage on our Website;</li>
+
+     
+     <li>COOKIES</li>
+
+<li>a. To optimize our web presence, we use cookies. These are small text files stored on your 
+computer's main memory. </li>
+<li>These cookies are deleted after you close the browser. </li>
+<li>Other cookies remain on your computer (long-term cookies) and permit its recognition on your next visit.</li>
+<li> This allows us to improve your access to our site.</li>
+<li> This helps us to learn more about your interests, and provides you with essential features and services
+</li>
+     
+
+     <li>SHARING OF PERSONAL INFORMATION</li>
+
+<li>We do not share your personal data with third parties without your prior consent other than:</li>
+     <li>To comply with laws or to respond to lawful requests and legal process</li>
+     <li> In an emergency, including to protect the personal safety of any person</li>
+     
+<li>CHILDREN</li>
+
+<li>If you are under 18, or the age of majority in the jurisdiction in which you reside, you may only use Our Website with the consent of your parent or legal guardian. In any case, We will not be liable for any cause of action that arose due to non-compliance with this section.</li>
+     
+     
+
+
+  <li>CHANGES TO THIS POLICY</li>
+
+<li>We may change this policy from time to time. If we make any changes to this policy, we will change the "Last Updated" date above. You agree that your continued use of our services after such changes have been published to our services will constitute your acceptance of such revised policy.</li>
+
+</ul>
+</div>
+
+)
+
+
+
+
+}
